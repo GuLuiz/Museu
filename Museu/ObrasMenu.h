a7e9dbc38643e7d7bbf6e972c69a1fb5401673cb
Nicollas Diniz <101371415+Dinizim@users.noreply.github.com>
2023-11-01 20:08:48 -0300

    OBRAS

--- Museu/ObrasMenu.h
+++ Museu/ObrasMenu.h
@@ -11,36 +11,6 @@ void obras()
     arquivo2 = fopen("Bilhete.txt", "r");
     int define;
     int sair = 0;
-<<<<<<< Updated upstream
-    system("cls");
-    printf("\n***********************************************");
-    printf("\n                  OBRAS");
-    printf("\n***********************************************\n");
-    printf("\n1 - Nikola Tesla");
-    printf("\n2 - O grito");
-    printf("\n3 - Santos Dumont");
-    printf("\n4 - 100 anos da arte moderna");
-    printf("\n0 - Sair");
-    printf("Escolha a obra desejada: ");
-    scanf("%d", &define);
-    switch (define)
-        while (sair == 0)
-        {
-            {
-            case 1:
-                system("start C:\\Museu\\Obras\\NikolaTesla.png");
-                break;
-            case 2:
-                system("start C:\\Museu\\Obras\\OGrito.png");
-                break;
-            case 3:
-                system("start C:\\Museu\\Obras\\SantosDumont.png");
-                break;
-            case 4:
-                system("start C:\\Museu\\Obras\\VanGogh.png");
-                break;
-=======
->>>>>>> Stashed changes
 
     printf("Por favor, digite seu Nome para prosseguimos :");
     fflush(stdin);
--- Museu/obrasAdm.h
+++ Museu/obrasAdm.h
@@ -5,39 +5,6 @@ int obrasAdm()
 
     int define;
     int sair = 0;
-<<<<<<< Updated upstream
-    
-    while (sair == 0){
-    system("cls");
-    printf("\n***********************************************");
-    printf("\n                  OBRAS");
-    printf("\n***********************************************\n");
-    printf("\n1 - Nikola Tesla");
-    printf("\n2 - O grito");
-    printf("\n3 - Santos Dumont");
-    printf("\n4 - 100 anos da arte mordena");
-    printf("\n0 - Sair");
-    printf("\nEscolha a obra desejada: ");
-    scanf("%d", &define);
-    switch (define)
-        {
-            case 0:
-                return 0;
-                break;
-            case 1:
-                system("start C:\\Museu\\Obras\\NikolaTesla.png");
-                break;
-            case 2:
-                system("start C:\\Museu\\Obras\\OGrito.png");
-                break;
-            case 3:
-                system("start C:\\Museu\\Obras\\SantosDumont.png");
-                break;
-            case 4:
-                system("start C:\\Museu\\Obras\\VanGogh.png");
-                break;
-=======
->>>>>>> Stashed changes
 
     while (sair == 0)
     {
